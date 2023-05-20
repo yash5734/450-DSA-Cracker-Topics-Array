7cab557e4451e38400862a305f1a6a190a7bb245
Yash Singhal <110597175+yash5734@users.noreply.github.com>
2023-05-20 21:56:44 +0530

    t

---
+++ 101. Symmetric Tree.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+
+    bool check(TreeNode* a,TreeNode* b){
+        if(a==NULL&&b==NULL) return true;
+        else if(a==NULL||b==NULL)return false;
+        return (a->val == b->val) && check(a->left,b->right) && check(a->right,b->left);
+    }
+
+    bool isSymmetric(TreeNode* root) {
+        if(root == NULL) return true;
+        if(check(root->left,root->right)){
+            return true;
+        }
+        return false;
+    
+    }
+};
