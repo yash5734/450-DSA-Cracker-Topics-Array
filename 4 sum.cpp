4167cf299ed60c3e7f967c5a51e8fe8a722472b2
Yash Singhal <110597175+yash5734@users.noreply.github.com>
2023-06-09 20:28:34 +0530

    array

---
+++ 4 sum.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    vector<vector<int>> fourSum(vector<int>& nums, int target) {
+        sort(nums.begin(),nums.end());
+        int n = nums.size();
+        vector<vector<int>>res;
+        for(int i=0;i<n;i++){
+            if (i > 0 && nums[i] == nums[i - 1]) continue;
+            // int k,l =0;
+            for(int j = i+1;j<n;j++){
+                if(j>i+1 && nums[j]==nums[j-1]) continue;
+            int k=j+1;
+            int l = n-1;
+           while (k < l) {
+                long long sum = nums[i];
+                sum += nums[j];
+                sum += nums[k];
+                sum += nums[l];
+                if (sum == target) {
+                    vector<int> temp = {nums[i], nums[j], nums[k], nums[l]};
+                    res.push_back(temp);
+                    k++; l--;
+
+                    //skip the duplicates:
+                    while (k < l && nums[k] == nums[k - 1]) k++;
+                    while (k < l && nums[l] == nums[l + 1]) l--;
+                }
+                else if (sum < target) k++;
+                else l--;
+            }
+            }
+        }
+        // vector<vector<int>>ans(res.begin(),res.end());
+        return res;
+    }
+};
