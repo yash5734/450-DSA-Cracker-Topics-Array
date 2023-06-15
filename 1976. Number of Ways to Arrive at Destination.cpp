267b1f72149c598e43c9e448e291edb0518c146e
Yash Singhal <110597175+yash5734@users.noreply.github.com>
2023-06-15 09:44:18 +0530

    Create 1976. Number of Ways to Arrive at Destination.cpp

---
+++ 1976. Number of Ways to Arrive at Destination.cpp
@@ -0,0 +1,56 @@
+class Solution {
+public:
+    int countPaths(int n, vector<vector<int>>& roads) {
+        // Creating an adjacency list for the given graph.
+        vector<pair<int, int>> adj[n];
+        for (auto it : roads) {
+            adj[it[0]].push_back({it[1], it[2]});
+            adj[it[1]].push_back({it[0], it[2]});
+        }
+
+        // Defining a priority queue (min heap).
+        priority_queue<pair<long long, int>,
+                       vector<pair<long long, int>>, greater<pair<long long, int>>> pq;
+
+        // Initializing the dist array and the ways array
+        // along with their first indices.
+        vector<long long> dist(n, 1e18), ways(n, 0);
+        dist[0] = 0;
+        ways[0] = 1;
+        pq.push({0, 0});
+
+        // Define modulo value
+        int mod = (int)(1e9 + 7);
+
+        // Iterate through the graph with the help of priority queue
+        // just as we do in Dijkstra's Algorithm.
+        while (!pq.empty()) {
+            long long dis = pq.top().first;
+            int node = pq.top().second;
+            pq.pop();
+
+            for (auto it : adj[node]) {
+                int adjNode = it.first;
+                int edW = it.second;
+
+                // This ‘if’ condition signifies that this is the first
+                // time we’re coming with this short distance, so we push
+                // in PQ and keep the no. of ways the same.
+                if (dis + edW < dist[adjNode]) {
+                    dist[adjNode] = dis + edW;
+                    pq.push({dis + edW, adjNode});
+                    ways[adjNode] = ways[node];
+                }
+
+                // If we again encounter a node with the same short distance
+                // as before, we simply increment the no. of ways.
+                else if (dis + edW == dist[adjNode]) {
+                    ways[adjNode] = (ways[adjNode] + ways[node]) % mod;
+                }
+            }
+        }
+        // Finally, we return the no. of ways to reach
+        // (n-1)th node modulo 10^9+7.
+        return ways[n - 1] % mod;
+    }
+};
