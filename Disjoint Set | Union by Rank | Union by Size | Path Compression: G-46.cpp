6a9bc43787d2c14de26427e6d76f87f0579ac4bf
Yash Singhal <110597175+yash5734@users.noreply.github.com>
2023-06-23 19:52:42 +0530

    graph

--- Disjoint Set | Union by Rank | Union by Size | Path Compression: G-46.cpp
+++ Disjoint Set | Union by Rank | Union by Size | Path Compression: G-46.cpp
@@ -5,10 +5,11 @@ class disJointSet{
     vector<int>rank,parent,size;
     disJointSet(int n){
         rank.resize(n+1,0);
-        size.resize(n+1,0);
+        size.resize(n+1);
         parent.resize(n+1);
         for(int i=0;i<=n;i++){
             parent[i] = i;
+            size[i] = i;
         }
     }
     
